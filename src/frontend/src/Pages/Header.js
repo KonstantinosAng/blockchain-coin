71427c7e52bf7958a71a491add1ca1ce024194a8
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-03-01 20:42:06 +0200

    :zap: added log out dispatch

--- src/frontend/src/Pages/Header.js
+++ src/frontend/src/Pages/Header.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import './Header.css';
+import { useStateValue } from '../extras/stateProvider.js';
+import { actionTypes } from '../extras/reducer.js';
 
 function Header() {
+
+  const [{user}, dispatch] = useStateValue();
+
+  const signOut = () => {
+    dispatch({
+      type: actionTypes.UNSET_USER
+    })
+  }
+
   return (
     <div className="header__root">
       <h3> Home </h3>
       <h3> Transact </h3>
       <h3> Profile </h3>
-      <h3> Log out </h3>
+      <h3 onClick={signOut}> Log out </h3>
     </div>
   )
 }
