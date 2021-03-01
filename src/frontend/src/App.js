cbe484dbaa6a2f21e485505d209192c6b15c5903
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-03-01 14:21:25 +0200

    :sparkles: migrated to react-flask app

---
+++ src/frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './Pages/Header.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/frontend/src/Pages/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header__root">
+      <h3> Home </h3>
+      <h3> Transact </h3>
+      <h3> Profile </h3>
+      <h3> Log out </h3>
+    </div>
+  )
+}
+
+export default Header
