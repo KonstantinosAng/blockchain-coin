270cb0cfc0d5480486627c3cc3be557e9150b45b
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-07-08 02:42:16 +0300

    :zap: format typo

--- src/frontend/src/Pages/Profile.js
+++ src/frontend/src/Pages/Profile.js
@@ -34,7 +34,7 @@ function Profile() {
         <div className="profile__root__column">
           <h2> {user.displayName} </h2>
           <h2> Signed name: {signedName} </h2>
-          <h2> Ammount: {ammount} cray coins </h2>
+          <h2> Amount: {ammount} cray coins </h2>
           <h2> Public Key </h2>
           <input value={user.publicKey} />
           <h2> Private Key </h2>
